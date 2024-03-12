c6926cb4ca796e9b1fa4d45cf154769db20f2789
Fzh <1303268167@qq.com>
2024-03-12 18:29:37 +0800

    new file:   .editorconfig 	new file:   .env.development 	new file:   .env.production 	new file:   .env.staging 	new file:   .eslintignore 	new file:   .eslintrc.js 	new file:   .gitignore 	new file:   .travis.yml 	new file:   LICENSE 	new file:   README-zh.md 	new file:   babel.config.js 	new file:   build/index.js 	new file:   jest.config.js 	new file:   jsconfig.json 	new file:   mock/index.js 	new file:   mock/mock-server.js 	new file:   mock/table.js 	new file:   mock/user.js 	new file:   mock/utils.js 	new file:   package.json 	new file:   postcss.config.js 	new file:   public/favicon.ico 	new file:   public/index.html 	new file:   src/App.vue 	new file:   src/api/table.js 	new file:   src/api/user.js 	new file:   src/assets/404_images/404.png 	new file:   src/assets/404_images/404_cloud.png 	new file:   src/components/Breadcrumb/index.vue 	new file:   src/components/Hamburger/index.vue 	new file:   src/components/SvgIcon/index.vue 	new file:   src/icons/index.js 	new file:   src/icons/svg/dashboard.svg 	new file:   src/icons/svg/example.svg 	new file:   src/icons/svg/eye-open.svg 	new file:   src/icons/svg/eye.svg 	new file:   src/icons/svg/form.svg 	new file:   src/icons/svg/link.svg 	new file:   src/icons/svg/nested.svg 	new file:   src/icons/svg/password.svg 	new file:   src/icons/svg/table.svg 	new file:   src/icons/svg/tree.svg 	new file:   src/icons/svg/user.svg 	new file:   src/icons/svgo.yml 	new file:   src/layout/components/AppMain.vue 	new file:   src/layout/components/Navbar.vue 	new file:   src/layout/components/Sidebar/FixiOSBug.js 	new file:   src/layout/components/Sidebar/Item.vue 	new file:   src/layout/components/Sidebar/Link.vue 	new file:   src/layout/components/Sidebar/Logo.vue 	new file:   src/layout/components/Sidebar/SidebarItem.vue 	new file:   src/layout/components/Sidebar/index.vue 	new file:   src/layout/components/index.js 	new file:   src/layout/index.vue 	new file:   src/layout/mixin/ResizeHandler.js 	new file:   src/main.js 	new file:   src/permission.js 	new file:   src/router/index.js 	new file:   src/settings.js 	new file:   src/store/getters.js 	new file:   src/store/index.js 	new file:   src/store/modules/app.js 	new file:   src/store/modules/settings.js 	new file:   src/store/modules/user.js 	new file:   src/styles/element-ui.scss 	new file:   src/styles/index.scss 	new file:   src/styles/mixin.scss 	new file:   src/styles/sidebar.scss 	new file:   src/styles/transition.scss 	new file:   src/styles/variables.scss 	new file:   src/utils/auth.js 	new file:   src/utils/get-page-title.js 	new file:   src/utils/index.js 	new file:   src/utils/request.js 	new file:   src/utils/validate.js 	new file:   src/views/404.vue 	new file:   src/views/dashboard/index.vue 	new file:   src/views/form/index.vue 	new file:   src/views/login/index.vue 	new file:   src/views/nested/menu1/index.vue 	new file:   src/views/nested/menu1/menu1-1/index.vue 	new file:   src/views/nested/menu1/menu1-2/index.vue 	new file:   src/views/nested/menu1/menu1-2/menu1-2-1/index.vue 	new file:   src/views/nested/menu1/menu1-2/menu1-2-2/index.vue 	new file:   src/views/nested/menu1/menu1-3/index.vue 	new file:   src/views/nested/menu2/index.vue 	new file:   src/views/table/index.vue 	new file:   src/views/tree/index.vue 	new file:   tests/unit/.eslintrc.js 	new file:   tests/unit/components/Breadcrumb.spec.js 	new file:   tests/unit/components/Hamburger.spec.js 	new file:   tests/unit/components/SvgIcon.spec.js 	new file:   tests/unit/utils/formatTime.spec.js 	new file:   tests/unit/utils/param2Obj.spec.js 	new file:   tests/unit/utils/parseTime.spec.js 	new file:   tests/unit/utils/validate.spec.js 	new file:   vue.config.js 	new file:   yarn.lock

---
+++ src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  title: '二手图书交易平台后台管理系统',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false
+}
